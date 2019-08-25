015a06fac05cf30dcb838c8ea9a6b02713fe3921
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-08-25 10:26:20 +0200

    running json schm wasm in deno

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,32 @@
-use jsonschema_valid::{is_valid, schemas::Draft7};
-use serde_json::{from_slice, from_str, Value};
+use jsonschema_valid::{is_valid as si_valid, schemas::Draft7};
+use serde_json::{from_slice, json, Value};
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-pub fn is_valid_str(instance: &str, schema: &str, validate_schema: i32) -> i32 {
-    let inst: Value = match from_str::<Value>(instance) {
-        Ok(inst) => inst,
-        _ => return 0,
-    };
-
-    let schm: Value = match from_str::<Value>(schema) {
-        Ok(schm) => schm,
-        _ => return 0,
-    };
-
-    is_valid(&inst, &schm, Some(&Draft7), validate_schema == 1) as i32
-}
+static NULL: &'static Value = &json!(null);
 
 #[wasm_bindgen]
-pub fn is_valid_buf(instance: &[u8], schema: &[u8], validate_schema: i32) -> i32 {
+pub fn is_valid(instance: &[u8], schema: &[u8], validate_schema: i32) -> i32 {    
     let inst: Value = match from_slice::<Value>(instance) {
-        Ok(inst) => inst,
+        Ok(inst) => {
+            if inst != *NULL {
+                inst
+            } else {
+                return 0
+            }
+        },
         _ => return 0,
     };
 
     let schm: Value = match from_slice::<Value>(schema) {
-        Ok(schm) => schm,
+        Ok(schm) => {
+            if schm != *NULL {
+                schm
+            } else {
+                return 0
+            }
+        },
         _ => return 0,
     };
 
-    is_valid(&inst, &schm, Some(&Draft7), validate_schema == 1) as i32
+    si_valid(&inst, &schm, Some(&Draft7), validate_schema == 1) as i32
 }
--- test.ts
+++ test.ts
@@ -1,19 +1,104 @@
 import { test, runIfMain } from "https://deno.land/std/testing/mod.ts";
-import { assert } from "https://deno.land/std/testing/asserts.ts";
-import { loadWasm } from "./mod.ts";
+import { assert, assertThrows } from "https://deno.land/std/testing/asserts.ts";
+import { isValid } from "./mod.ts";
 
-const { is_valid_str, is_valid_buf } = loadWasm();
+const encoder: TextEncoder = new TextEncoder();
 
 test({
-  name: "fraud world",
+  name: "hello fraud",
   fn() {
-    const isValid = is_valid_str(
-      '{"fraud":419}',
-      '{"type":"object","properties":{"fraud":{"type":"number"}}}',
-      1
-    );
+    assert(   isValid('419', '{"type":"number"}')   )
+  }
+})
 
-    assert(Boolean(isValid));
+test({
+  name: "null pointer bug",
+  fn() {
+    assertThrows((): void =>  { isValid('null', '{"type":"number"}')}   )
+  }
+})
+
+test({
+  name: "validating json buffers",
+  fn() {
+    assert(
+      isValid(
+        encoder.encode('{"fraud":419}'),
+        encoder.encode(
+          '{"type":"object","properties":{"fraud":{"type":"number"}}}'
+        )
+      )
+    );
+  }
+});
+
+test({
+  name: "encoding strings is slow",
+  fn() {
+    assert(
+      isValid(
+        '{"fraud":419}',
+        '{"type":"object","properties":{"fraud":{"type":"number"}}}'
+      )
+    );
+  }
+});
+
+test({
+  name: "validating the input schema slows things down too",
+  fn() {
+    assert(
+      isValid(
+        '{"fraud":419}',
+        '{"type":"object","properties":{"fraud":{"type":"number"}}}',
+        true
+      )
+    );
+  }
+});
+
+test({
+  name: "detects schema syntax errors if validateSchema is true",
+  fn() {
+    assert(
+      !isValid(
+        '{"fraud":419}',
+        '{"type":"object","properties":{"fraud":{"type":"numbr"}}}',
+        true
+      )
+    );
+  }
+});
+
+test({
+  name: "does not detect schema syntax errors if validateSchema is false",
+  fn() {
+    assert(
+      isValid(
+        '{"fraud":419}',
+        '{"type":"object","properties":{"fraud":{"type":"numbr"}}}',
+        false
+      )
+    );
+  }
+});
+
+test({
+  name: "validateSchema defaults to true",
+  fn() {
+    assert(
+      !isValid(
+        '{"fraud":419}',
+        '{"type":"object","properties":{"fraud":{"type":"numbr"}}}'
+      )
+    );
+  }
+});
+
+test({
+  name: "gracefully handles invalid json",
+  fn() {
+    assert(!isValid("//", "{}"));
   }
 });
 
