7871401d23aa2392d75ac9acb2dd83a63ea70dba
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-08-30 10:04:55 +0200

    correct ci

--- check.sh
+++ check.sh
@@ -12,7 +12,7 @@ fails=$(deno run --allow-read ./test.ts 2>&1 | grep "failures" | tr -d -c 0-9)
 
 echo "[check.sh] 🏁 # of fails: $fails"
 
-if [[ $fails = 0 ]]; then
+if [[ $fails -eq 0 ]]; then
   exit 0;
 else  
   exit 1;
